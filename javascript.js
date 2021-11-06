cfd4f98c967ab8a3ad0e93ef179de43346b0a65c
akbaradam24 <adam.akhbar24@gmail.com>
2021-11-06 20:49:46 +0900

    navbar

--- javascript.js
+++ javascript.js
@@ -2,3 +2,12 @@ function darkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode")
 }
+
+const menuToggle = document.querySelector('.menu-toggle input');
+const headerRight = document.querySelector('.header-right');
+
+menuToggle.addEventListener('click', function() {
+    headerRight.classList.toggle('slide');
+});
+
+
